451c3b8e6dd37ae99ac8d4598151ecd5f210a39a
timkrebs <timkrebs9@gmail.com>
2024-01-04 21:22:10 +0000

    Version1.0.1 - Bug fixed

---
+++ src/gateway/server.py
@@ -0,0 +1,14 @@
+import os, gridfs, pika, json
+from flask import Flask, request, send_file
+from flask_pymongo import PyMongo
+from auth import validate
+from auth_svc import access
+from storage import util
+from bson.objectid import ObjectId
+
+server = Flask(__name__)
+server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/scripts"
+
+mongo = PyMongo(server)
+fs = gridfs.GridFS(mongo.db)
+
