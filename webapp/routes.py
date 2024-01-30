c0444e19d3d4bf1cc658338070eb39c5350f2d19
timkrebs <timkrebs9@gmail.com>
2024-01-30 17:26:05 +0100

    moved templates to new location

---
+++ webapp/routes.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Request, Depends
+from auth.dependencies import ProtectedEndpoint
+
+from config import templates
+
+
+webapp_router = APIRouter()
+
+
+@webapp_router.get("/")
+def home(request: Request):
+
+    return templates.TemplateResponse(
+        "home.html",
+        {
+            "request": request
+        }
+    )
+
+
+@webapp_router.get("/profile", dependencies=[Depends(ProtectedEndpoint)])
+def profile(request: Request):
+
+    return templates.TemplateResponse(
+        "profile.html",
+        {
+            "request": request,
+            "userinfo": request.session['userinfo']
+        }
+    )
