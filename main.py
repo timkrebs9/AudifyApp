f94750155d3d0da90e4f1ca8ab7d0c251a6e6855
timkrebs <timkrebs9@gmail.com>
2024-01-31 08:32:03 +0100

    modified main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import asyncio
+import uvicorn
 from auth.routes import auth_router
 from webapp.routes import webapp_router
 from storage.routes import storage_router
@@ -8,9 +10,17 @@ app.include_router(webapp_router)
 app.include_router(auth_router)
 app.include_router(storage_router)
 
-def main():
-    import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+#def main():
+#    import uvicorn
+#    uvicorn.run(app, host="0.0.0.0", port=8000)
+#
+#if __name__ == '__main__':
+#    main()
 
-if __name__ == '__main__':
-    main()
+async def main():
+    config = uvicorn.Config("main:app", port=8000, log_level="info")
+    server = uvicorn.Server(config)
+    await server.serve()
+
+if __name__ == "__main__":
+    asyncio.run(main())
