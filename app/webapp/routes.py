f1c6d76a27f6a93168c9129fb59514fad9397ba2
timkrebs <timkrebs9@gmail.com>
2024-02-12 17:00:28 +0100

    depending the state of the user navbar shows specific option for the enduser

--- app/webapp/routes.py
+++ app/webapp/routes.py
@@ -1,5 +1,7 @@
+from app.auth.dependencies import protected_endpoint
 from app.core.config import templates
 from fastapi import APIRouter
+from fastapi import Depends
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 
@@ -7,7 +9,7 @@ from fastapi.responses import HTMLResponse
 webapp_router = APIRouter(tags=["Home"])
 
 
-@webapp_router.get("/profile")
+@webapp_router.get("/profile", dependencies=[Depends(protected_endpoint)])
 def profile(request: Request) -> HTMLResponse:
     return templates.TemplateResponse(
         "profile.html",
