28a8b5912a02573042dc8cc22d09cd1d4dbdd96c
timkrebs <timkrebs9@gmail.com>
2024-02-12 19:50:59 +0100

    Added Middleware and secret retrival

--- app/main.py
+++ app/main.py
@@ -1,22 +1,25 @@
 import uvicorn
 from app.auth.routes import auth_router
+from app.core.config import secrets
 from app.core.config import templates
 from app.webapp.routes import webapp_router
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.sessions import SessionMiddleware
 
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.add_middleware(SessionMiddleware, secret_key=secrets["WEBAPP_SECRET_KEY"])
 
 app.include_router(webapp_router)
 app.include_router(auth_router)
 
 
 @app.get("/", response_class=HTMLResponse)
-async def index(request: Request) -> HTMLResponse:
+async def home(request: Request) -> HTMLResponse:
     print("Request for index page received")
     return templates.TemplateResponse("home.html", {"request": request})
 
