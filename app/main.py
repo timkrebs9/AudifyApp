071c463d51ccfa55f5ea3135201198a48782cdea
timkrebs <timkrebs9@gmail.com>
2024-02-13 08:32:18 +0100

    Running uvicorn with proxy-headers enabled

--- app/main.py
+++ app/main.py
@@ -26,4 +26,6 @@ async def home(request: Request) -> HTMLResponse:
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run(
+        "main:app", host="0.0.0.0", port=8000, proxy_headers=True, reload=True
+    )
